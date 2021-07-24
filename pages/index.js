8396117cd39fadc241f1afbb9d49b26a728f92e9
tsvikash <87815944+tsvikash@users.noreply.github.com>
2021-07-24 18:34:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Vikash Sharma</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
